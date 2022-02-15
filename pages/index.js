f3d449033e73d15646d4aa5c1412e4203ad829e5
zadragon <mase0804@naver.com>
2022-02-16 01:19:50 +0900

    코스 완료

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,19 @@ import NavBar from "../components/NavBar";
 import Seo from "../components/Seo";
 import Link from "next/link";
 import {useEffect, useState} from "react";
+import {useRouter} from "next/router";
 
-const API_KEY = "8ae094c58fd51942547548792dae285d"
-
-export default function Home() {
-    const [movies, setMovies] = useState([]);
-    useEffect(() => {
-        (async () => {
-            const {results} = await (await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)).json();
-            console.log(results);
-            setMovies(results)
-        })();
-    }, [])
+export default function Home({results}) {
+    const router = useRouter();
+    const onClick = (id, title) => {
+        router.push(`/movies/${title}${id}`)
+    }
     return (
         <div className="container">
             <Seo title="Home"></Seo>
-            {!movies && <h4>Loading...</h4>}
-            {movies?.map((movie) => (
-                <div key={movie.id}>
-                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+            {results?.map((movie) => (
+                <div onClick={() => onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
+                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}/>
                     <h4>
                         <Link href={`/movies/${movie.original_title}/${movie.id}`}>
                             <a>{movie.original_title}</a>
@@ -29,30 +23,43 @@ export default function Home() {
                 </div>
             ))}
             <style jsx>{`
-                .container {
-                  display: grid;
-                  grid-template-columns: 1fr 1fr;
-                  padding: 20px;
-                  gap: 20px;
-                }
-                .movie {
-                  cursor: pointer;
-                }
-                .movie img {
-                  max-width: 100%;
-                  border-radius: 12px;
-                  transition: transform 0.2s ease-in-out;
-                  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-                }
-                .movie:hover img {
-                  transform: scale(1.05) translateY(-10px);
-                }
-                .movie h4 {
-                  font-size: 18px;
-                  text-align: center;
-                }
-              `}
+              .container {
+                display: grid;
+                grid-template-columns: 1fr 1fr;
+                padding: 20px;
+                gap: 20px;
+              }
+
+              .movie {
+                cursor: pointer;
+              }
+
+              .movie img {
+                max-width: 100%;
+                border-radius: 12px;
+                transition: transform 0.2s ease-in-out;
+                box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+              }
+
+              .movie:hover img {
+                transform: scale(1.05) translateY(-10px);
+              }
+
+              .movie h4 {
+                font-size: 18px;
+                text-align: center;
+              }
+            `}
             </style>
         </div>
     )
+}
+
+export async function getServerSideProps() {
+    const {results} = await (await fetch(`http://localhost:3000/api/moives`)).json();
+    return {
+        props: {
+            results
+        }
+    }
 }
